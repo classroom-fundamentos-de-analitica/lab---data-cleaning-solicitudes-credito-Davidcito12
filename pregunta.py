489009c7424440856e6de2427fc6ab22c5354b71
David Lopez <92931033+Davidcito12@users.noreply.github.com>
2022-11-13 00:40:15 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,26 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import re
 import pandas as pd
-
-
+from datetime import datetime
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df = df.dropna(axis=0)
+    df = df.drop_duplicates()
+    
+    df = df.applymap(lambda x: re.sub(r'[-_]'," ",str(x).lower()))
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "").replace({ r"[\D]" : '' },regex= True).astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    
+    def dateparse(date):
+        kek = date.split("/")
+        if len(kek[0]) == 4:
+            return datetime(int(kek[0]),int(kek[1]),int(kek[2]))
+        else:
+            return datetime(int(kek[2]),int(kek[1]),int(kek[0]))
+    
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(dateparse)
+    
+    df.drop_duplicates(inplace = True)
     return df
